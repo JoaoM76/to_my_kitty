2dcda092ceb743277c51f5f4ba8df69e8ed6a19c
JoaoM76 <128715871+JoaoM76@users.noreply.github.com>
2024-12-15 00:57:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Função para atualizar o texto
+function updateText() {
+    const textContainer = document.querySelector(".text-container");
+    textContainer.innerHTML = `
+      <h1>iaiii minha garota, espero que tenha gostado dessa surpresinha, primeiro, eu te amo demais, você é incrivel mesmo, nunca duvide disso,
+      você tem uma maturidade incrivel é a mulher mais inteligente que conheço,
+      além de ser uma tremenda gostosa, linda perfeita que me faz perder o fôlego só de lembrar dos seus detalhes, 
+      você é especial demais pode contar comigo sempre, amo tudo em você, me impressiona sempre cada detalhe e 
+      também o fato que tu é boa em tudo que faz ( ͡° ͜ʖ ͡°) kkkkk te amo minha gatinha.</h1>
+    `;
+  }
+  
