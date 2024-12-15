21a21fa9315600f9bfd5a74cce470dee5f868b99
JoaoM76 <128715871+JoaoM76@users.noreply.github.com>
2024-12-15 01:04:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function updateText() {
       você tem uma maturidade incrivel é a mulher mais inteligente que conheço,
       além de ser uma tremenda gostosa, linda perfeita que me faz perder o fôlego só de lembrar dos seus detalhes, 
       você é especial demais pode contar comigo sempre, amo tudo em você, me impressiona sempre cada detalhe e 
-      também o fato que tu é boa em tudo que faz ( ͡° ͜ʖ ͡°) kkkkk te amo minha gatinha.</h1>
+      também o fato que tu é boa em tudo que faz   ( ͡° ͜ʖ ͡°) kkkkk te amo minha gatinha.</h1>
     `;
   }
   
